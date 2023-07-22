d2da54f2c3f2d40dbacac8c6300005eb3de887dd
Naveen160299 <naveenkumar160299@gmail.com>
2023-07-22 15:17:10 +0530

    Prabha

--- src/org/ext/Sample.java
+++ src/org/ext/Sample.java
@@ -1,7 +1,10 @@
 package org.ext;
 
 public class Sample {
+private void Prabha() {
+	System.out.println("Prabha");
 
+}
 public static void main(String[] args) {
    System.out.println("naveen");
    System.out.println("kavin");
